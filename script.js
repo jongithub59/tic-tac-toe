5f52222a7983169ae65a3ef5f76e70c69ecdf529
jongithub59 <jdelrealj01@gmail.com>
2023-12-20 10:58:01 -0600

    game now playabe in console with no win conditions

--- script.js
+++ script.js
@@ -5,35 +5,32 @@ function createBoard() {
 
     for (let i = 0; i < squares; i++) {
         board.push(Box())
-
     }
 
     const getBoard = () => board //returns the array as board
 
     const showBoard = () => { //displays the play area and which squares are marked by each player
         const boardValues = board.map((cell) =>  cell.getMarker()) 
-        console.log(boardValues)
+        const showBoardValues = boardValues.join("|").replace(/([^|]+?\|[^|]+?\|[^|]+?)\|/g, "$1\n"); //new array specifically for display that adds "|" as a separator and makes a new line after 3 elements to represent a tic tac toe board
+        console.log(showBoardValues)
     }
 
     const placeMarker = (cell, player) => { //takes in an array location and current player to place their marker
-        const unmarkedBoxes = board.filter((cell) => cell.getMarker() == 0) //returns an array with all elements with a marker of zero, or unmarked
+        const unmarkedBoxes = board.filter((cell) => cell.getMarker() == '-') //returns an array with all elements with a marker of zero, or unmarked
 
         if (!unmarkedBoxes.length) return
 
-        if (unmarkedBoxes.includes(cell)) {
-            cell.addMarker(player) //adds the marker of the current player to the given array location 
-        } else
-            console.log(`Square already marked`)
+        if (unmarkedBoxes.includes(board[cell]))  //checks that the selected array index is not alredy marked
+            board[cell].addMarker(player) //adds the marker of the current player to the given array location 
 
     }
     
-
     return { getBoard, showBoard, placeMarker } //need to return any method or variable here to be used outside ex: board.getBoard works but now board.squares
 }
 
 //create individual cells for the board
 function Box() {
-    let marker = 0
+    let marker = '-'
 
     const addMarker = (player) => { //sets the array element's marker value to whatever the current player is
         marker = player
@@ -50,17 +47,19 @@ function createPlayers(playerOne = 'Player One', playerTwo = 'Player Two') { //c
     const players = [
         {
         name: playerOne,
-        marker: 1
+        marker: 'O'
     },
      {
         name: playerTwo,
-        marker: 2
+        marker: 'X'
      }
     ]
     return {players} //returns the array; need to access the array first, then the player objects within
 }
 
 function createGame() {
+    const board = createBoard() //grants access to methods of createBoard 
+    const gameBoard = board.getBoard() //grants access to the board array through this variable
     const players = createPlayers()
     const playerOne = players.players[0] 
     const playerTwo = players.players[1]
@@ -82,7 +81,10 @@ function createGame() {
     }
 
     const playRound = function(cell) {
-        console.log(`${getTurn().name} marks square ${gameBoard.indexOf(cell) + 1}`)
+        console.log(`${getTurn().name} marks square ${cell}`)
+        cell = cell - 1 //makes number line up with array indexes example: inputting 1 will mark index 0, the first array element
+
+        if (gameBoard[cell].getMarker() != '-') return console.log(`Square ${cell + 1} already marked, try again`) //stops the loop if the selectd square is marked by a player and let them retry
 
         board.placeMarker(cell, getTurn().marker) //accesses the board object, then runs the placeMarker method on the board array method given when calling playRound() and also taking the current active player's marker to use
 
@@ -90,21 +92,14 @@ function createGame() {
         currentTurn()
 
     }
-    const board = createBoard() //grants access to methods of createBoard 
 
     currentTurn()
-
-    const gameBoard = board.getBoard() //grants access to the board array thourgh this variable
-
-
-    playRound(gameBoard[1])
-    playRound(gameBoard[3])
-
-    // board.showBoard()
-
+    
+    return { playRound, getTurn, nextTurn }
 }
 
-createGame()
+const game = createGame() //game can be played in the console by typing game.playRound() and inputting the square number
+
 
 
 
